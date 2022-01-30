350cc80c378a045c3526d6ef7c99d00525e089d1
Justin Roy <justin.roy.tamaela@mendix.com>
2022-01-30 20:47:19 +0100

    commit for array cardio

--- apps/html_canvas/main.js
+++ apps/html_canvas/main.js
@@ -12,10 +12,13 @@ ctx.lineWidth = "100";
 let isDrawing = false;
 let lastX = 0;
 let lastY = 0;
+let hue = 0;
+let direction = true;
 
 function draw(e) {
   if (!isDrawing) return;
   console.log(e);
+  ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
   ctx.beginPath();
   // start from
   ctx.moveTo(lastX, lastY);
@@ -24,6 +27,19 @@ function draw(e) {
   ctx.stroke();
   lastX = e.offsetX;
   lastY = e.offsetY;
+
+  hue++;
+  if (hue >= 360) {
+    hue = 0;
+  }
+  if (ctx.lineWidth >= 100 || ctx.lineWidth <= 1) {
+    direction = !direction;
+  }
+  if (direction) {
+    ctx.lineWidth++;
+  } else {
+    ctx.lineWidth--;
+  }
 }
 
 canvas.addEventListener("mousedown", (e) => {
